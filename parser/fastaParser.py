7d85389f9f8d2e13bfdf8cde88428e23c56e76cb
QG02 <qaysgh02@gmail.com>
2025-06-17 12:39:42 -0500

    Added function to check the type of content in a file

--- parser/fastaParser.py
+++ parser/fastaParser.py
@@ -9,7 +9,8 @@ def parseFastaInput():
     fastaPath = input("Enter the path to your FASTA file: ").strip()
     metadataPath = input("Enter the path to your metadata JSON file: ").strip()
     typeMap = fileType(metadataPath)
-    return parseFasta(fastaPath, typeMap)
+    sequences = parseFasta(fastaPath, typeMap)
+    return fastaPath, sequences
 
 # Function to extract data from JSON file
 def fileType(jsonPath):
@@ -38,3 +39,34 @@ def parseFasta(filePath, typeMap):
     for record in SeqIO.parse(filePath, "fasta"):
         sequences[record.id] = record.seq
     return sequences
+
+def fileIdentifier(filePath):
+    desc = []
+    # open and read the file 
+    with open(filePath, "r") as f:
+        for record in SeqIO.parse(f,"fasta"):
+            header = record.description
+            label = classifySeq(header)
+            desc.append((header,label))
+    return desc
+    # extract header from records
+    # run a conditional to see what kind of information it is
+
+def classifySeq(header):
+    if "mRNA" in header or header.startswith("NM_") or header.startswith("XM_"):
+        if header.startswith("NM_"):
+            return "experimentally curated mRNA"
+        else:
+            return "computationally modeled mRNA"
+    elif "protein" in header or header.startswith("NP_") or header.startswith("XP_"):
+         if header.startswith("NP_"):
+            return "protein from experimentally curated mRNA"
+         else:
+            return "protein from computationally curated mRNA"
+    elif "chromosome=" in header:
+        if ":c" in header:
+            return "genomic DNA - complement strand"
+        else:
+            return "genomic DNA - original strand"
+    else:
+        return "unclassified"
